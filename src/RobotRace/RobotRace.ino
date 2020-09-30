10ab14d99abd0db80722a14b146f08547b1bc0d5
Matthew Rothlisberger <mattjrothlis@gmail.com>
2020-09-29 22:53:08 -0400

    initial commit

---
+++ src/RobotRace/RobotRace.ino
@@ -0,0 +1,40 @@
+#include <Servo.h>
+
+Servo right;
+Servo left;
+Servo steer;
+
+void setup() {
+  // initalizing all servos
+  right.attach(9);
+  steer.attach(10);
+  left.attach(11);
+  
+  // makes sure start values are correct
+  right.write(90);
+  steer.write(85);
+  left.write(90);
+}
+
+void loop() {
+  delay(3000); // 3 second delay
+  race(); // calling function to race
+  while (1) {
+    // ensures that race function doesn't loop
+  }
+}
+
+void race() {
+  // steers straight and runs both wheels at full speed
+  right.write(110);
+  steer.write(85);
+  left.write(70);
+
+  delay(10000); // times out 10 seconds for the race
+
+  //stops wheels at end of race period
+  right.write(90);
+  steer.write(85);
+  left.write(90);
+}
+
---
+++ src/SteeringTest/SteeringTest.ino
@@ -0,0 +1,56 @@
+#include <Servo.h>
+
+Servo right;
+Servo left;
+Servo steer;
+
+void setup() {
+  right.attach(9);
+  steer.attach(10);
+  left.attach(11);
+
+  right.write(90);
+  steer.write(85);
+  left.write(90);
+}
+
+void loop() {
+  // running the wheels at 110 and 70 
+  // lets them spin 5 times per second
+  
+  steer.write(85;
+  right.write(110);
+  left.write(70);
+
+  delay(1000);
+  
+  right.write(110);
+  steer.write(85);
+  left.write(70);
+
+  delay(3000);
+  
+  right.write(110);
+  left.write(70);
+
+  delay(3000);
+  
+  right.write(110);
+  left.write(70);
+
+  delay(3000);
+  
+  right.write(110);
+  left.write(70);
+
+  delay(3000);
+  
+  right.write(110);
+  left.write(70);
+
+  delay(3000);
+  
+  while (1) {
+    
+  }
+}
